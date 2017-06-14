5baefb8ac0c6857e4f3c11b994244a746f49f27c
YuriyVlasenko <yuriyvlasenko89@gmail.com>
2017-06-14 15:07:25 +0300

    add weather forecast

--- compiled.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var singleton = require('./singleton');
-
-console.log(singleton);
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 import singleton from './singleton';
+import forecastManager from './forecastManager'
 
+// task 1
 console.log('singleton');
 console.log(singleton);
 
+// task 2
+console.log('forecastManager');
+
+const cities = ['Kharkiv', 'Kiev'];
+
+// parallel start
+for (let cityName of cities){
+    forecastManager.getCityWeather(cityName).then((weather)=> {
+        console.log(`${cityName} weather`);
+        console.log(weather);
+    })
+}
+
+// queue
+const [kharkiv, kiev] = cities;
+forecastManager.getCityWeather(kharkiv)
+    .then((weather)=> {
+            console.log(`${kharkiv} weather`);
+            console.log(weather);
+            return forecastManager.getCityWeather(kiev);
+        })
+    .then((weather)=>{
+        console.log(`${kiev} weather`);
+        console.log(weather);
+    });
 
